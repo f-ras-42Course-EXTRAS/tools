5e587b234f7a6367f95968781be7d4283c4924d8
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-06 00:55:19 +0200

    Script update

--- prepare_eval.sh
+++ prepare_eval.sh
@@ -6,15 +6,20 @@
 #    By: fras <fras@student.codam.nl>                 +#+                      #
 #                                                    +#+                       #
 #    Created: 2023/05/05 03:44:02 by fras          #+#    #+#                  #
-#    Updated: 2023/05/05 05:47:52 by fras          ########   odam.nl          #
+#    Updated: 2023/05/06 00:38:40 by fras          ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
+# Save starting directory 
+starting_directory=$(echo "$PWD")
+
+# Get copy directory from command-line arguments
 source_directory=${@:1:$#-1}
 destination_directory=${@:$#}
 
+# Check if command-line arguments are valid
 if [ $# -lt 2 ];
 	then
 		echo Choose directory to copy files 'from' and 'to'. 
@@ -29,7 +34,26 @@ if [ $# -lt 2 ];
 		exit 1;
 	fi
 
+# Start script
 cp -r $source_directory $destination_directory \
 	&& find $destination_directory -name '.*' | xargs rm -rf
+cp .gitignore $destination_directory
 
-echo "Copying files <$source_directory> to.. <$destination_directory>"
+# echo "Copying files <$source_directory> to <$destination_directory>"
+# cd $destination_directory
+# echo Changing directory to $destination_directory.
+
+# echo Starting to initialize git.
+# git init
+# echo Enter the Git remote link to setup upload for eval:
+# read git_repository
+# git remote add origin $git_repository
+# echo Repository remote added succesfully..
+# git add .
+# git commit -m "project done"
+# git push
+# echo Files pushed, operation done.
+# cd $starting_directory
+# echo Going back to starting directory.
+# rm -rf $starting_directory
+# echo Copied files deleted.
